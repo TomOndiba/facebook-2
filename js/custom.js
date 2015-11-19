161fcb67505ae4a5e6813c356a8ff18397a9aac4
Rony <faridrony55@gmail.com>
2015-11-19 23:59:50 +0600

    facebook
    responsive facebook by bootstrap

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+document.onkeydown = function (cc) {
+if(cc.which == 85){
+return false;
+}
+}
+
+document.onkeydown = function (cc) {
+if(cc.which == 85){
+return false;
+}
+if(cc.which == 80){
+return false;
+}
+}
